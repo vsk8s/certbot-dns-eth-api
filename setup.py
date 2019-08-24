3e1698563a12d54de044ff703de93bd3ae59d935
Maximilian Falkenstein <maximilian.falkenstein@sos.ethz.ch>
2019-08-24 18:50:44 +0200

    [Setup]: Rebuild GRPC from proto

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
-from setuptools import setup
+#!/usr/bin/env python3
+import os
+import re
+import shutil
+
+from setuptools import setup, Command
 from setuptools import find_packages
+from setuptools.command.build_py import build_py
 
 install_requires = [
     'acme>=0.29.0',
@@ -7,7 +13,43 @@ install_requires = [
     'setuptools',
     'zope.interface',
     'grpcio',
+    'grpcio-tools',
 ]
+
+
+# See https://stackoverflow.com/a/54236739/3589716
+class GrpcTool (Command):
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        import grpc_tools.protoc
+
+        cwd = os.getcwd()
+        os.chdir(cwd + '/proto')
+
+        grpc_tools.protoc.main([
+            'grpc_tools.protoc',
+            '--python_out=../certbot_dns_eth_api',
+            '--grpc_python_out=../certbot_dns_eth_api',
+            'dnsapi.proto'
+        ])
+
+        os.chdir(cwd)
+        with open('certbot_dns_eth_api/dnsapi_pb2_grpc.py', 'r+') as file:
+            data = file.read()
+            file.seek(0)
+            file.write(re.sub("import dnsapi_pb2", "import certbot_dns_eth_api.dnsapi_pb2", data))
+            file.truncate()
+
+
+class BuildPyCommand (build_py):
+    def run(self):
+        self.run_command('grpc')
+        super(BuildPyCommand, self).run()
 
 setup(
     name = 'certbot-dns-eth-api',
@@ -20,6 +62,10 @@ setup(
         ],
     },
     packages = find_packages(),
+    cmdclass = {
+      "build_py": BuildPyCommand,
+      "grpc": GrpcTool,
+    },
 )
 
 
